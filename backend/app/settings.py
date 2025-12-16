98d21a86f37069fc1cdc7b115ffe3935da40d9de
Jeremiah Brown <jeremiah.1582@googlemail.com>
2025-12-16 14:56:33 +0100

    made sdjustments to Dockerfile to mount DB in the root on host and in Data/ on container

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'backend', '0.0.0.0']
+
 # For production, replace the above with your actual production domain(s).
 
 
@@ -60,11 +61,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
 ]
 
 # CORS settings for frontend
-CORS_ALLOWED_ORIGINS =os.environ.get('CORS_ALLOWED_ORIGINS').split(',')
+CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS').split(',')
+
+
 
 
 # For development only - allow all origins
@@ -97,7 +99,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR /'db.sqlite3',
     }
 }
 
